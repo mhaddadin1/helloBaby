448e41267ba107d4e0631d76f2225b89811852b0
mousa haddadin <mousahaddadin@gmail.com>
2022-08-12 18:58:44 -0500

    added some nav functionality for login and logout

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,38 +1,54 @@
 import React from "react";
 import "./style.css";
 import { Nav, Navbar } from "react-bootstrap";
-// import { ReactComponent as Logo } from "./logo.svg";
+import Auth from "../../utils/auth";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Menu = () => {
-  return (
-    <Navbar
-      className="nav1"
-      collapseOnSelect
-      expand={false}
-      bg="dark"
-      variant="dark"
-    >
-      <Navbar.Brand href="/profile">
-        {/* <Logo
-          alt=""
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        /> */}
-        HelloBaby
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/profile">Profile</Nav.Link>
-          <Nav.Link href="/logs">Log Feeding</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+  if (Auth.loggedIn()) {
+    return (
+      <Navbar
+        className="nav1"
+        collapseOnSelect
+        expand={false}
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand href="/profile">HelloBaby</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/profile">Profile</Nav.Link>
+            <Nav.Link href="/logs">Log Feeding</Nav.Link>
+            <Nav.Link href="/login" onClick={() => Auth.logout()}>
+              Logout
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  } else {
+    return (
+      <Navbar
+        className="nav1"
+        collapseOnSelect
+        expand={false}
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand href="/profile">HelloBaby</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
+            {/* <Nav.Link href="/profile">Profile</Nav.Link> */}
+            {/* <Nav.Link href="/logs">Log Feeding</Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 };
 
 export default Menu;
